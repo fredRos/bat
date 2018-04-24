e25e8d7d62d1e3220f0e30c604b4be934bcc7241
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2018-04-24 15:01:23 +0200

    [tools] Add doc-bat.sh
    * enable testing
    * `nprocs` -> `nproc`

--- tools/test-bat.sh
+++ tools/test-bat.sh
@@ -5,7 +5,6 @@
 ./autogen.sh
 ./configure --with-cuba=download --enable-roostats --enable-parallel CXXFLAGS='-Wall -Wextra -pedantic -Werror'
 # build html reference guide, manual in html and pdf
-make -j`nprocs` && make -C doc/manual/ pdf
-# make -j`nprocs` distcheck VERBOSE=1
+make -j`nproc` distcheck VERBOSE=1
 
 echo "OK"
